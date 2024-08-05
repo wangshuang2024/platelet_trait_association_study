6a6f23d6b60d6b8dcf628840e09eb3afd1ab2c9b
wangshuang2024 <147407943+wangshuang2024@users.noreply.github.com>
2024-08-05 21:53:12 +0800

    Create STAARpipelineSummary_Sliding_Window_Summary.r

---
+++ STAARpipelineSummary_Sliding_Window_Summary.r
@@ -0,0 +1,76 @@
+###########################################################
+# Summarization and visualization of sliding window
+# analysis results using STAARpipelineSummary
+# Xihao Li, Zilin Li
+# Initiate date: 11/04/2021
+# Current date: 03/10/2022
+###########################################################
+rm(list=ls())
+gc()
+
+## load required packages
+library(gdsfmt)
+library(SeqArray)
+library(SeqVarTools)
+library(STAAR)
+library(STAARpipeline)
+library(STAARpipelineSummary)
+
+###########################################################
+#           User Input
+###########################################################
+## Number of jobs for each chromosome
+jobs_num <- get(load("/GWAS/data/nov_gwas_staar_update/step0_preSTAAR/jobs_num.Rdata"))
+## aGDS directory
+agds_dir <- get(load("/GWAS/data/nov_gwas_staar_update/step0_preSTAAR/agds_dir.Rdata"))
+### Known loci
+known_loci <- get(load("/GWAS/data/nov_gwas_staar_update/plt/plt_known_loci_pruning/plt_known_loci_genome_LD_pruning.Rdata"))
+## Null model
+obj_nullmodel <- get(load("/GWAS/data/nov_gwas_staar_update/plt/snv/step1_fitnullmodel/plt_snv_nullmodel_GENESIS.Rdata"))
+
+## results path
+input_path <- "/GWAS/data/nov_gwas_staar_update/plt/snv/snv_5mask/step6_sliding_window_maf5percent/"
+output_path <- "/GWAS/data/nov_gwas_staar_update/plt/snv/snv_5mask/summary/step6_sliding_window_maf5percent_1/"
+## results name
+sliding_window_results_name <- "plt_snv_Sliding_Window"
+
+## QC_label
+QC_label <- "annotation/filter"
+## geno_missing_imputation
+geno_missing_imputation <- "mean"
+## method_cond
+method_cond <- "optimal"
+## variant_type
+variant_type <- "SNV"
+## alpha level
+alpha <- 0.05
+
+## Annotation_dir
+Annotation_dir <- "annotation/info/FunctionalAnnotation"
+## Annotation channel
+Annotation_name_catalog <- get(load("/GWAS/data/nov_gwas_staar_update/step0_preSTAAR/Annotation_name_catalog.Rdata"))
+# Or equivalently
+# Annotation_name_catalog <- read.csv("/path_to_the_file/Annotation_name_catalog.csv")
+## Use_annotation_weights
+Use_annotation_weights <- TRUE
+## Annotation name
+Annotation_name <- c("CADD","LINSIGHT","FATHMM.XF","aPC.EpigeneticActive","aPC.EpigeneticRepressed","aPC.EpigeneticTranscription",
+                     "aPC.Conservation","aPC.LocalDiversity","aPC.Mappability","aPC.TF","aPC.Protein")
+
+
+cMAC_cutoff <- 5
+rare_maf_cutoff <- 0.05
+
+source("/all_Rscript/Sliding_Window_Results_Summary.R")
+###########################################################
+#           Main Function 
+###########################################################
+Sliding_Window_Results_Summary(agds_dir=agds_dir,jobs_num=jobs_num,
+                               input_path=input_path,output_path=output_path,sliding_window_results_name=sliding_window_results_name,
+                               obj_nullmodel=obj_nullmodel,known_loci=known_loci,cMAC_cutoff=cMAC_cutoff,rare_maf_cutoff=rare_maf_cutoff,
+                               method_cond=method_cond,
+                               QC_label=QC_label,geno_missing_imputation=geno_missing_imputation,variant_type=variant_type,
+                               Annotation_dir=Annotation_dir,Annotation_name_catalog=Annotation_name_catalog,
+                               Use_annotation_weights=Use_annotation_weights,Annotation_name=Annotation_name,
+                               alpha=alpha,manhattan_plot=TRUE,QQ_plot=TRUE)
+
